b44819157a9af2491ad8caf6fd402ce3edcdccd4
cpatil07 <93142447+cpatil07@users.noreply.github.com>
2021-11-17 16:57:52 +0530

    Update JhooqDockerDemoController.java
    change Print statement

--- src/main/java/com/jhooq/demo/Jhooqdockerdemo/JhooqDockerDemoController.java
+++ src/main/java/com/jhooq/demo/Jhooqdockerdemo/JhooqDockerDemoController.java
@@ -8,6 +8,6 @@ public class JhooqDockerDemoController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Docker Demo - Welcom to Montran DevOPS";
+        return "Docker Demo - Welcom to Montran DevOPS!!";
     }
 }
